3a989590e1d5c19e5b03bdb039a8c2798eaaa861
MichaelWkTran <Michael.wktran@gmail.com>
2024-05-20 09:05:09 +1200

    Added placeholder sprites

---
+++ Assets/RopeBuilder2D.cs
@@ -0,0 +1,46 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RopeBuilder2D : MonoBehaviour
+{
+    [SerializeField] HingeJoint2D m_segementPrefab;
+    [SerializeField] float m_segmentOffset;
+    [SerializeField] int m_segementCount;
+    [SerializeField] bool m_axis;
+
+    public void SpawnSegment()
+    {
+        HingeJoint2D lastSegment = null; try { lastSegment = GetComponentsInChildren<HingeJoint2D>()[^1]; } catch (Exception) { }
+        HingeJoint2D segment = Instantiate(m_segementPrefab, transform);
+
+        if (lastSegment == null) return;
+
+        segment.transform.position = lastSegment.transform.position;
+        segment.transform.position += (m_axis ? Vector3.right : -Vector3.up) * m_segmentOffset;
+        segment.connectedBody = lastSegment.attachedRigidbody;
+    }
+
+    public void SpawnSegments()
+    {
+        for (int i = 0; i < m_segementCount; i++) SpawnSegment();
+    }
+}
+
+[CustomEditor(typeof(RopeBuilder2D))]
+public class RopeBuilder2DEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RopeBuilder2D ropeBuilder = (RopeBuilder2D)target;
+        if (GUILayout.Button("Spawn Segments")) ropeBuilder.SpawnSegments();
+    }
+}
+
+#endif
+
---
+++ Assets/RopeLineRenderer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeLineRenderer : MonoBehaviour
+{
+    public LineRenderer m_lineRenderer;
+    public Transform m_segmentContainer;
+    public float m_capsuleLength;
+    public bool m_axis;
+    
+    //public void Init()
+    //{
+    //    List<Vector3> positions = new List<Vector3>();
+    //    foreach (Transform segment in m_segmentContainer)
+    //    {
+    //        positions
+    //        positions.Add(segment.position);
+    //    }
+    //    
+    //    if (m_capsuleLength <= 0) return;
+    //
+    //}
+    //
+    //void Start()
+    //{
+    //    Init();
+    //}
+    //
+    //void Update()
+    //{
+    //    
+    //}
+}
