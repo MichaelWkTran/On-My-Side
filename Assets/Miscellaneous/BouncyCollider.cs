e0a261856bab9428e9936ecf1efcf3a04144bcc9
MichaelWkTran <88115414+MichaelWkTran@users.noreply.github.com>
2024-07-23 18:07:41 +1200

    Added new sprites

--- Assets/Miscellaneous/BouncyCollider.cs
+++ Assets/Miscellaneous/BouncyCollider.cs
@@ -5,6 +5,9 @@ public class BouncyCollider : MonoBehaviour
     public float m_bounceScale = 1.0f;
     public float m_minImpulse = 0.0f;
     public float m_maxImpulse = 1.0f;
+    public float m_tweenDuration;
+    public float m_tweenScale;
+    public SpriteRenderer m_sprite;
 
     void OnCollisionEnter2D(Collision2D _collision)
     {
@@ -16,5 +19,10 @@ public class BouncyCollider : MonoBehaviour
         Vector2 bounceImpulse = -_collision.contacts[0].normal * impulseMagnitude;
 
         rb.AddForce(bounceImpulse, ForceMode2D.Impulse);
+
+        //Trigger the bounce animation
+        m_sprite.transform.localScale = Vector3.one;
+        LeanTween.cancel(m_sprite.gameObject);
+        LeanTween.scale(m_sprite.gameObject, Vector3.one * m_tweenScale, m_tweenDuration).setEasePunch();
     }
 }
